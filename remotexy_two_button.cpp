23391463a64b0d04df6ced75798a397c90aeecec
diariesofijon <86806697+diariesofijon@users.noreply.github.com>
2022-01-06 20:39:00 +0300

    Update remotexy_two_button.cpp

--- remotexy_two_button.cpp
+++ remotexy_two_button.cpp
@@ -1,7 +1,7 @@
 // https://remotexy.com/en/editor/c7090379d49a7892149ecba7d7b44e7f/
 
 /*
-   -- viktors pult with double button --
+   -- viktors pult with double button by nikita --
    
    This source code of graphical user interface 
    has been generated automatically by RemoteXY editor.
@@ -36,24 +36,21 @@
 // RemoteXY configurate  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
-  { 255,11,0,0,0,173,0,14,13,0,
+  { 255,11,0,11,0,135,0,14,13,0,
   2,0,38,4,22,11,2,26,31,31,
   79,78,0,79,70,70,0,1,0,4,
   5,12,12,2,31,88,0,129,0,3,
   19,13,6,17,115,116,97,114,116,0,
-  129,0,41,50,34,3,17,208,184,208,
-  183,208,188,208,181,208,189,208,184,209,
-  130,209,140,32,208,189,208,176,208,191,
-  209,128,208,176,208,178,208,187,208,181,
-  208,189,208,184,208,181,0,1,0,9,
-  43,12,12,2,31,88,0,1,0,82,
-  42,12,12,2,31,88,0,1,0,82,
-  4,12,12,2,31,88,0,129,0,81,
-  18,13,6,17,115,116,111,112,0,8,
-  0,20,23,17,17,24,5,0,47,17,
-  30,30,2,26,31,129,0,5,56,18,
-  6,17,108,101,102,116,0,129,0,78,
-  56,18,6,17,114,105,103,104,116,0 };
+  1,0,9,43,12,12,2,31,88,0,
+  1,0,82,42,12,12,2,31,88,0,
+  1,0,82,4,12,12,2,31,88,0,
+  129,0,81,18,13,6,17,115,116,111,
+  112,0,8,0,20,23,17,17,24,5,
+  0,47,17,30,30,2,26,31,129,0,
+  5,56,18,6,17,108,101,102,116,0,
+  129,0,78,56,18,6,17,114,105,103,
+  104,116,0,67,4,51,50,20,5,2,
+  26,11 };
   
 // this structure defines all the variables and events of your control interface 
 struct {
@@ -67,6 +64,9 @@ struct {
   float compass_vector; // 0 .. 359.999 
   int8_t joystick_1_x; // =-100..100 x-coordinate joystick position 
   int8_t joystick_1_y; // =-100..100 y-coordinate joystick position 
+
+    // output variables
+  char text_1[11];  // string UTF8 end zero 
 
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
